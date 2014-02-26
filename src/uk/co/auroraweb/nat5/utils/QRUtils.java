7472749ed925b0561ea73ee33641131743b48987
Adam Hirst <contact@aurora.co.uk>
2014-02-26 18:57:27 +0000

    Implemented QR Generator
    A QR generator was added to allow fans emails to be converted into QR
    codes.

--- src/uk/co/auroraweb/nat5/utils/QRUtils.java
+++ src/uk/co/auroraweb/nat5/utils/QRUtils.java
@@ -1,12 +1,29 @@
 //Utils for QR codes (Encoding)
+//TODO: Testing; comments
 
 package uk.co.auroraweb.nat5.utils;
 
+import java.awt.Image;
+import java.io.IOException;
+import java.net.URL;
+
+import javax.imageio.ImageIO;
+
 public class QRUtils {
 	
-	public QRUtils(String msg) {
+	public static Image generateQRCode(String msg) {
+		URL url;
+		Image image;
 		
-		//TODO: Generate QR Code
+		try {
+			url = new URL("https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" + msg);
+			image = ImageIO.read(url);
+			return image;
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return null;
+		}
 		
 	}
 	
