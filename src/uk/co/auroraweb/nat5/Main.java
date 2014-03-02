74f41d2d4107b438a808153c5b30dac8f8bfba5b
Adam Hirst <contact@aurora.co.uk>
2014-03-02 19:41:15 +0000

    Added Splash screen

--- src/uk/co/auroraweb/nat5/Main.java
+++ src/uk/co/auroraweb/nat5/Main.java
@@ -3,15 +3,29 @@
 
 package uk.co.auroraweb.nat5;
 
+import java.awt.Cursor;
+
 import uk.co.auroraweb.nat5.gui.GuiMain;
+import uk.co.auroraweb.nat5.gui.GuiSplashScreen;
 
 public class Main {
 	
 	public static void main(String[] args) {
 		
-		//Load the main gui on load.
-		new GuiMain();
-	
+		GuiSplashScreen splash = new GuiSplashScreen();
+		
+		try {
+			splash.setCursor (Cursor.getPredefinedCursor(Cursor.WAIT_CURSOR));
+			Thread.sleep(5000);
+			splash.setCursor (Cursor.getPredefinedCursor(Cursor.DEFAULT_CURSOR));
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		splash.dispose();
+		
+	    new GuiMain();
 	}
-	
+		
 }
---
+++ src/uk/co/auroraweb/nat5/gui/GuiSplashScreen.java
@@ -0,0 +1,30 @@
+package uk.co.auroraweb.nat5.gui;
+
+
+import java.awt.Dimension;
+import java.net.URL;
+
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
+import javax.swing.JWindow;
+
+public class GuiSplashScreen extends JWindow {
+
+	private static final long serialVersionUID = 1L;
+	
+	public GuiSplashScreen() {
+		
+		URL imgURL = getClass().getResource("/res/splash.jpg");
+		ImageIcon img = new ImageIcon(imgURL);
+		JLabel lblImg = new JLabel("", img, JLabel.CENTER);
+		
+		pack();
+		setSize(new Dimension(500, 200));
+		setLocationRelativeTo(null);
+		add(lblImg);
+		pack();
+		setVisible(true);
+		
+	}
+	
+}
